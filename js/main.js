8de9161b4dbbd0a9cf0c93ebce365f87e074c013
jhriyaz <jhriyaz@gmail.com>
2023-08-13 21:01:36 +0600

    added first card js codes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
-let countValue1 = 0
+let countValue = 0
+let rangeProgress = document.getElementById('rangeProgress1')
 document.getElementById('btnIncrement1').addEventListener('click', function () {
-    countValue1 += 1;
-    let rangeProgress = document.getElementById('rangeProgress1')
-    if (countValue1 <= 33) {
-        document.getElementById('numberCount1').innerText = countValue1;
-        rangeProgress.value = countValue1;
+    countValue += 1;
+    if (countValue <= 33 && countValue >= 1) {
+        document.getElementById('numberCount1').innerText = countValue;
+        rangeProgress.value = countValue;
         rangeProgress.classList.replace('range-error', 'range-primary')
     }
-    if (countValue1 === 33) {
+    if (countValue === 33) {
         rangeProgress.classList.replace('range-primary', 'range-success')
     }
-    if (countValue1 > 33) {
+    if (countValue > 33) {
         alert('please start next Tasbeeh')
+        countValue = 33
+    }
+    if (countValue < 1) {
+        alert('please count must be more than 0')
+    }
+})
+document.getElementById('btnDecrement1').addEventListener('click', function () {
+    countValue -= 1;
+    if (countValue <= 33 && countValue >= 0) {
+        document.getElementById('numberCount1').innerText = countValue;
+        rangeProgress.value = countValue;
+        rangeProgress.classList.replace('range-success', 'range-primary')
+    }
+    if (countValue === 0) {
+        rangeProgress.classList.replace('range-primary', 'range-error')
+    }
+    if (countValue > 33) {
+        alert('please start next Tasbeeh')
+    }
+    if (countValue < 0) {
+        alert('please count must be more than 0')
+        countValue = 0
     }
 })
