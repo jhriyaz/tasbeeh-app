8e1529f308103b3f40e5280d980af6205aa1f7a7
jhriyaz <jhriyaz@gmail.com>
2023-08-13 20:05:07 +0600

    Increment button code done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('js file external')
+let countValue1 = 0
+document.getElementById('btnIncrement1').addEventListener('click', function () {
+    countValue1 += 1;
+    let rangeProgress = document.getElementById('rangeProgress1')
+    if (countValue1 <= 33) {
+        document.getElementById('numberCount1').innerText = countValue1;
+        rangeProgress.value = countValue1;
+        rangeProgress.classList.replace('range-error', 'range-primary')
+    }
+    if (countValue1 === 33) {
+        rangeProgress.classList.replace('range-primary', 'range-success')
+    }
+    if (countValue1 > 33) {
+        alert('please start next Tasbeeh')
+    }
+})
