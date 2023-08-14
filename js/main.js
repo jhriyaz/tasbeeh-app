5aa3cd2e699fa14d0d31d94db102271779923e8e
jhriyaz <jhriyaz@gmail.com>
2023-08-14 19:52:07 +0600

    added sound efect and notification also removed alert

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
 let countValue1 = 0
+let autoPLay=document.getElementById('increamentPlay')
+let decrementAutoPLay=document.getElementById('decreamentPlay')
+let resetAutoPLay=document.getElementById('resetPlay')
 let rangeProgress1 = document.getElementById('rangeProgress1')
+let errorContent = document.getElementById('errorContent')
+let errorBox = document.getElementById('errorBox')
+
+// #762d2d4a
+// green-- #00800047
+
 document.getElementById('btnIncrement1').addEventListener('click', function () {
     countValue1 += 1;
+    autoPLay.currentTime=0
+    autoPLay.play()
     if (countValue1 <= 33) {
         document.getElementById('numberCount1').innerText = countValue1;
         rangeProgress1.value = countValue1;
         rangeProgress1.classList.replace('range-error', 'range-primary')
+       
     }
     if (countValue1 === 33) {
         rangeProgress1.classList.replace('range-primary', 'range-success')
+        errorContent.innerText='please start saying " আলহামদুলিল্লাহ "'
+        errorContent.style.background='#00800047'
+        errorBox.classList.remove('hidden')
+      
     }
     if (countValue1 > 33) {
-        alert('please start saying আলহামদুলিল্লাহ')
+       
         countValue1 = 33
+        
     }
 })
 document.getElementById('btnDecrement1').addEventListener('click', function () {
     countValue1 -= 1;
+    autoPLay.currentTime=0
+    decrementAutoPLay.play()
     if (countValue1 >= 0) {
         document.getElementById('numberCount1').innerText = countValue1;
         rangeProgress1.value = countValue1;
@@ -26,7 +45,9 @@ document.getElementById('btnDecrement1').addEventListener('click', function () {
         rangeProgress1.classList.replace('range-primary', 'range-error')
     }
     if (countValue1 < 0) {
-        alert('please count must be more than 0')
+        errorContent.innerText='say " সুবহানাল্লাহ " and start clicking Increment'
+        errorContent.style.background='#762d2d4a'
+        errorBox.classList.remove('hidden')
         countValue1 = 0
     }
 })
@@ -37,25 +58,35 @@ let countValue2 = 0
 let rangeProgress2 = document.getElementById('rangeProgress2')
 document.getElementById('btnIncrement2').addEventListener('click', function () {
     countValue2 += 1;
+    autoPLay.currentTime=0
+    autoPLay.play()
     if(countValue1!==33){
         countValue2=0
-        return alert('say সুবহানাল্লাহ 33 times first')
+        errorBox.classList.remove('hidden')
+        errorContent.innerText='say " সুবহানাল্লাহ " 33 times first';
+        errorContent.style.background='#762d2d4a'
+        return
+        
     }
     if (countValue2 <= 33) {
         document.getElementById('numberCount2').innerText = countValue2;
         rangeProgress2.value = countValue2;
         rangeProgress2.classList.replace('range-error', 'range-primary')
+       
     }
     if (countValue2 === 33) {
         rangeProgress2.classList.replace('range-primary', 'range-success')
     }
     if (countValue2 > 33) {
-        alert('please start saying আল্লাহু আকবার')
+        errorContent.innerText='please start saying " আল্লাহু আকবার "'
+        errorContent.style.background='#00800047'
         countValue2 = 33
     }
 })
 document.getElementById('btnDecrement2').addEventListener('click', function () {
     countValue2 -= 1;
+    autoPLay.currentTime=0
+    decrementAutoPLay.play()
     if (countValue2 >= 0) {
         document.getElementById('numberCount2').innerText = countValue2;
         rangeProgress2.value = countValue2;
@@ -65,7 +96,9 @@ document.getElementById('btnDecrement2').addEventListener('click', function () {
         rangeProgress2.classList.replace('range-primary', 'range-error')
     }
     if (countValue2 < 0) {
-        alert('please count must be more than 0')
+        errorContent.innerText='say " আলহামদুলিল্লাহ " and start clicking Increment';
+        errorContent.style.background='#762d2d4a'
+        errorBox.classList.remove('hidden')
         countValue2 = 0
     }
 })
@@ -77,10 +110,14 @@ let countValue3 = 0
 let rangeProgress3 = document.getElementById('rangeProgress3')
 document.getElementById('btnIncrement3').addEventListener('click', function () {
     countValue3 += 1;
+    autoPLay.currentTime=0
+    autoPLay.play()
     if(countValue2!==33){
-        countValue3=0
-       return alert('say আলহামদুলিল্লাহ 33 times first');
-        
+        errorBox.classList.remove('hidden')
+       errorContent.innerText='say " আলহামদুলিল্লাহ " 33 times first';
+       errorContent.style.background='#762d2d4a'
+       countValue3=0
+       return
     }
     if (countValue3 <= 34) {
         document.getElementById('numberCount3').innerText = countValue3;
@@ -89,14 +126,17 @@ document.getElementById('btnIncrement3').addEventListener('click', function () {
     }
     if (countValue3 === 34) {
         rangeProgress3.classList.replace('range-primary', 'range-success')
+        errorContent.innerText='please click on reset button'
+        errorContent.style.background='#00800047'
     }
     if (countValue3 > 34) {
-        alert('please click on reset button')
         countValue3 = 34
     }
 })
 document.getElementById('btnDecrement3').addEventListener('click', function () {
     countValue3 -= 1;
+    autoPLay.currentTime=0
+    decrementAutoPLay.play()
     if (countValue3 >= 0) {
         document.getElementById('numberCount3').innerText = countValue3;
         rangeProgress3.value = countValue3;
@@ -106,7 +146,9 @@ document.getElementById('btnDecrement3').addEventListener('click', function () {
         rangeProgress3.classList.replace('range-primary', 'range-error')
     }
     if (countValue3 < 0) {
-        alert('please count must be more than 0')
+        errorContent.innerText='say " আল্লাহু আকবার " and start clicking Increment';
+        errorContent.style.background='#762d2d4a'
+        errorBox.classList.remove('hidden')
         countValue3 = 0
     }
 })
@@ -115,6 +157,8 @@ document.getElementById('btnDecrement3').addEventListener('click', function () {
 
 // reset Button
 document.getElementById('resetBtn').addEventListener('click',function(){
+    resetAutoPLay.currentTime=0
+    resetAutoPLay.play()
     countValue1=0
     countValue2=0
     countValue3=0
